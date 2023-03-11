ced387dad981dabb2c24f38ad65a42d2677ec4af
Jumpei Mori <nikutto.kom@gmail.com>
2023-03-11 17:08:50 +0900

    Use react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
 import HelloPage from "./component/HelloPage";
+import {BrowserRouter, Route, Routes} from "react-router-dom"
 
 const App = () => (
-    <div className="App">
-      <HelloPage />      
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/hello"
+          element={<HelloPage />}
+        />
+        <Route
+          path="/*"
+          element={<div>Not Found</div>}
+        />
+      </Routes>
+    </BrowserRouter>
+    
 );
 
 export default App;
